dfcf17d6c92016df631aa666f143f871852f21b5
ganggang <just_send@163.com>
2012-09-24 22:10:13 +0800

    初始化

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jxdc::Application.config.secret_token = '75dc07b373cf43b15b9cca4d81129b819398c895b8e5a2daf136c0735e4b3e02d527d8e8374b333a63694dee893fd78762333163fc36ee0164bec5069229bce2'
